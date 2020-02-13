8783e5a8d69933b9413004463ec74e500bc0852f
felixcatto <felix0808@yandex.ru>
2020-02-13 06:42:30 +0400

    improve cqrt

--- sicp/chapter1/08.rkt
+++ sicp/chapter1/08.rkt
@@ -4,27 +4,23 @@
 
 (define accuracy 0.1)
 
-(define (improveGuess n guess)
-  (/
-    (+
-      (/ n (* guess guess))
-      (* 2 guess))
-    3))
-
-(define (isGoodEnough oldGuess newGuess)
-  (<
-    (abs (- oldGuess newGuess))
-    accuracy))
-
-(define (iter n guess)
-  (define newGuess (improveGuess n guess))
-  (if
-    (isGoodEnough guess newGuess)
-    newGuess
-    (iter n newGuess)))
-
 (define (icqrt n)
-  (iter n 1.0))
+  (define (improveGuess guess)
+    (/
+      (+
+        (/ n (* guess guess))
+        (* 2 guess))
+      3))
+  (define (isGoodEnough oldGuess newGuess)
+    (<
+      (abs (- oldGuess newGuess))
+      accuracy))
+  (define (iter guess)
+    (define newGuess (improveGuess guess))
+    (if
+      (isGoodEnough guess newGuess)
+      newGuess
+      (iter newGuess)))
+  (iter 1.0))
 
-(check-within (improveGuess 8.0 1) 3.33 accuracy)
 (check-within (icqrt 8) 2 accuracy)
